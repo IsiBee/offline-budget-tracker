d1692576179264666429e021552ada990663c76c
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-04-25 09:38:32 -0500

    added idb.js and starter code

---
+++ public/js/idb.js
@@ -0,0 +1,63 @@
+let db;
+
+const request = indexedDB.open('offline_expenses', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('new_expense', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        uploadExpense();
+    }
+};
+
+request.onerror = function (event) {
+    console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+    const expenseObjectStore = transaction.objectStore('new_expense');
+
+    expenseObjectStore.add(record);
+};
+
+function uploadExpense() {
+    const transaction = db.transaction(['new_expense'], 'readwrite');
+
+    const expenseObjectStore = transaction.objectStore('new_expense');
+
+    const getAll = expenseObjectStore.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    const transaction = db.transaction(['new_expense'], 'readwrite');
+                    const expenseObjectStore = transaction.objectStore('new-expense');
+                    expenseObjectStore.clear();
+
+                    alert('All saved expenses have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    }
+};
+
+window.addEventListener('online', uploadExpense);
